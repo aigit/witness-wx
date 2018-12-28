55ea9a5aa3669a45113eaf521151282b9acd6ed8
Dylan <liudelong5@126.com>
2018-12-28 12:48:03 +0800

    init

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    appTitle: '相册回收站',
+
+    /** 通讯域名 */
+    coshost: 'https://sh.file.myqcloud.com/files/v2',
+    basePath: '/applet/album',
+    BUCKET_NAME:'/witness1',
+    APP_ID:'/1255425548',
+    DIR_NAME:'/w1',
+    MAP_TYPE:'gcj02',
+    WITNESS_SERVER_HOST:'https://www.iwitness.top',
+    BROADCAST_URL:'https://www.iwitness.top/scene/broadcast',
+    COS_DOWNLOAD_HOST:"http://witness1-1255425548.cossh.myqcloud.com"
+};
